78f963e13ed89ef7d51fa85159b58cea31fe0fc6
Kontolistik <151897038+Hexamouse@users.noreply.github.com>
2024-12-04 14:16:42 +0700

    fix guild - FactionPoint

---
+++ app/Guild/FactionPoint/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import Navbar from '@/app/components/Navbar';
+import Footer from '@/app/components/Footer';
+
+export default function FactionPoint() {
+    return (
+        <div>
+            <Navbar />
+
+            <Footer />
+        </div>
+    );
+}
